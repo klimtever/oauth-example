cb45a6aade004a81eedfacb28bd42f2035b97c1c
Hans Van Beneden <hans.vanbeneden@esaturnus.com>
2018-01-18 17:39:28 +0100

    added restTemplate implementation

--- my-website/src/main/java/com/example/mywebsite/config/OAuth2LoginConfig.java
+++ my-website/src/main/java/com/example/mywebsite/config/OAuth2LoginConfig.java
@@ -2,12 +2,19 @@ package com.example.mywebsite.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestOperations;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
+import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+
+import java.util.ArrayList;
 
 /**
  * The OAuth2 login configuration for My Website.
@@ -36,6 +43,27 @@ public class OAuth2LoginConfig {
                 .userNameAttributeName("user")
                 .clientName("my auth server")
                 .build();
+    }
+
+    @Bean
+    public OAuth2RestOperations restOperations(OAuth2ClientContext oauth2ClientContext) {
+        return new OAuth2RestTemplate(resource(), oauth2ClientContext);
+    }
+
+    private OAuth2ProtectedResourceDetails resource() {
+        ClientRegistration myauthClient = myWebsiteClientRegistration();
+        AuthorizationCodeResourceDetails resource = new AuthorizationCodeResourceDetails();
+        resource.setScope(new ArrayList<>(myauthClient.getScopes()));
+        resource.setAccessTokenUri(myauthClient.getProviderDetails().getTokenUri());
+        resource.setUserAuthorizationUri(myauthClient.getProviderDetails().getAuthorizationUri());
+        resource.setClientId(myauthClient.getClientId());
+        resource.setClientSecret(myauthClient.getClientSecret());
+        resource.setPreEstablishedRedirectUri(myauthClient.getRedirectUriTemplate());
+        resource.setUseCurrentUri(false);
+        resource.setUserAuthorizationUri(myauthClient.getProviderDetails().getUserInfoEndpoint().getUri());
+        resource.setId(myauthClient.getClientId());
+        resource.setGrantType(AuthorizationGrantType.AUTHORIZATION_CODE.getValue());
+        return resource;
     }
 
 }
--- my-website/src/main/java/com/example/mywebsite/controller/MyWebsiteController.java
+++ my-website/src/main/java/com/example/mywebsite/controller/MyWebsiteController.java
@@ -1,8 +1,10 @@
 package com.example.mywebsite.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestOperations;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.time.LocalDateTime;
@@ -18,11 +20,26 @@ public class MyWebsiteController {
     @Value("${time.url}")
     private String timeUrl;
 
+    // Inject the RestOperations. This will be an instance of org.springframework.security.oauth2.client.OAuth2RestTemplate
+    // which is made available as a Bean by our Oauth2LoginConfig.
+    @Autowired
+    private RestOperations restOperations;
+
     @RequestMapping("/time")
     public ModelAndView time() {
         ModelAndView mav = new ModelAndView("time");
-        mav.addObject("currentTime", LocalDateTime.now());
+        mav.addObject("currentTime", getCurrentTime());
         return mav;
     }
 
+    /**
+     * Get the time from an external REST API using the injected RestOperations.
+     */
+    private String getCurrentTime() {
+        try {
+            return restOperations.getForObject(timeUrl, String.class);
+        } catch (Exception e) {
+            return e.getMessage();
+        }
+    }
 }
